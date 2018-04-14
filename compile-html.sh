ea4a963595ef1c46d331bdf65ab09c7df1c746f3
Robin Richtsfeld <robin.richtsfeld@gmail.com>
2018-04-14 16:23:59 +0200

    Move include logic into standalone file

--- compile-html.sh
+++ compile-html.sh
@@ -6,47 +6,9 @@ DIR="$(cd "$(dirname "$0")" && pwd)"
 mkdir -p "$(dirname tmp/$file)"
 cp "src/$file" "tmp/$file.tr"
 
-directives_raw=$(grep -ho '<!--#include "[^"]\+"-->' "tmp/$file.tr")
-mapfile -t directives <<< "$directives_raw"
-dependencies=()
-dirty=false
-
-for directive in "${directives[@]}"; do
-  [[ ${directive} =~ ^'<!--#include "'([^\"]+)'"-->'$ ]]
-  
-  path_raw="${BASH_REMATCH[1]}"
-  
-  if [[ "$path_raw" == css/* ]] || [[ "$path_raw" == js/* ]]; then
-    path="bin/$path_raw"
-  else
-    path="src/$path_raw"
-  fi
-  
-  dependency="$directive~$path"
-  dependencies+=("$dependency")
-  
-  if ! "$DIR"/checksum-read.sh "$path"; then
-    dirty=true
-  fi
-done
-
-if ! "$DIR"/checksum-read.sh "tmp/$file.tr"; then
-  dirty=true
-  "$DIR"/checksum-write.sh "tmp/$file.tr"
-fi
-
-if [[ "$dirty" == false ]]; then
+if ! "$DIR"/compile-includes.sh "$file"; then
   exit 0
 fi
-
-for dependency in "${dependencies[@]}"; do
-  IFS='~' dependency=($dependency)
-  directive="${dependency[0]}"
-  path="${dependency[1]}"
-  include_raw=$(tr '\n' '\r' < "$path")
-  include=$(echo "$include_raw" | sed 's~\(&\|\\\)~\\\1~g')
-  sed -i -- "s~$directive~$include~" "tmp/$file.tr"
-done
 
 tr '\r' '\n' < "tmp/$file.tr" > "tmp/$file"
 htmlmin --keep-optional-attribute-quotes "tmp/$file" "tmp/$file.min"
