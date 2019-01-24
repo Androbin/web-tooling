3ca280ac4efedd697fc116954d4b7600bce4cfdc
Robin Richtsfeld <robin.richtsfeld@gmail.com>
2019-01-24 03:45:14 +0100

    Digestify asset file names

--- compile.py
+++ compile.py
@@ -55,6 +55,16 @@ changed_assets = get_changed(all_assets)
 deps_assets = set(dep[0] for dep in meta.deps if dep[1] in changed_assets)
 changed_docs |= all_docs.intersection(deps_assets)
 
+def suffix_path(path, suffix):
+  ext = path[path.rfind('.'):]
+  return path[:-len(ext)] + '-' + suffix + ext
+
+def digestify_path(path):
+  return suffix_path(path, digest[path].hex()[:7])
+
+def cleanup_path(path):
+  return suffix_path(path, r'[a-z0-9]' * 7)
+
 def process(paths):
   for path in paths:
     ext = path[path.rfind('.'):]
@@ -64,19 +74,52 @@ def process(paths):
 
     if ext in ['.css']:
       processed = csscompress(raw)
+      pathOut = digestify_path(path)
+      cleanup = glob('bin/' + cleanup_path(path))
     elif ext in ['.js', '.json']:
-      processed = jsmin(raw, quote_chars="'\"`")
+      processed = process_js(raw)
+      pathOut = digestify_path(path)
+      cleanup = glob('bin/' + cleanup_path(path))
     else:
       processed = process_doc(path, raw)
+      pathOut = path
+      cleanup = []
 
     os.makedirs(os.path.dirname('bin/' + path), exist_ok=True)
 
-    with open('bin/' + path, 'w+') as file:
+    for dirt in cleanup:
+      os.remove(dirt)
+
+    with open('bin/' + pathOut, 'w+') as file:
       file.write(processed)
 
     print(path)
+    print(pathOut, file=sys.stderr)
+
+def process_js(raw):
+  references = re.finditer(r'/[*][*]/(["\']/?)([^"\']+)(["\'])/[*][*]/', raw)
+
+  for match in references:
+    reference = match.group(0)
+    prefix = match.group(1)
+    path = match.group(2)
+    suffix = match.group(3)
+
+    raw = raw.replace(reference, prefix + digestify_path(path) + suffix)
+
+  return jsmin(raw, quote_chars="'\"`")
 
 def process_doc(pathSelf, raw):
+  dependencies = re.finditer(r'(href|src)="/([^"]*\.(css|js|json))"', raw)
+
+  for match in dependencies:
+    dependency = match.group(0)
+    prop = match.group(1)
+    path = match.group(2)
+
+    raw = raw.replace(dependency, prop + '="/' + digestify_path(path) + '"')
+    meta.deps.add((pathSelf, path))
+
   return htmlmin(
     process_includes(pathSelf, raw),
     remove_comments=True,
@@ -90,8 +133,9 @@ def process_includes(pathSelf, rawSelf):
     directive = match.group(0)
     path = match.group(1)
 
-    is_asset = path.split('/', 1)[0] in ['css', 'js']
-    path = 'bin/' + path if is_asset else 'src/' + path
+    ext = path[path.rfind('.'):]
+    is_asset = ext in ['.css', '.js', '.json']
+    path = 'bin/' + digestify_path(path) if is_asset else 'src/' + path
 
     with open(path, 'r') as file:
       raw = file.read()
@@ -101,8 +145,29 @@ def process_includes(pathSelf, rawSelf):
 
   return rawSelf
 
+def process_htaccess():
+  with open('src/.htaccess', 'r') as file:
+    raw = file.read()
+
+  preloads = re.finditer(r'(Header add Link "<https?://[^/]+/)((css|js)/[^>]+)(>; rel=preload; as=[a-z]+" env=X_[A-Z_]+)', raw)
+
+  for match in preloads:
+    preload = match.group(0)
+    prefix = match.group(1)
+    path = match.group(2)
+    suffix = match.group(4)
+    raw = raw.replace(preload, prefix + digestify_path(path) + suffix)
+
+  with open('bin/.htaccess', 'w+') as file:
+    file.write(raw)
+
+  print('.htaccess', file=sys.stderr)
+
 process(changed_assets)
 process(changed_docs)
+
+if (changed_assets | changed_docs):
+  process_htaccess()
 
 meta.digest.update(digest)
 
